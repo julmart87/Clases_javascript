31d97c52545b7be342d07252a94c22ca30afc198
julian martinez <julian1987m@gmail.com>
2024-05-02 11:14:34 -0300

    practicas

--- clase0/js/script.js
+++ clase0/js/script.js
@@ -2,6 +2,20 @@
 // let salida = entrada + " " + "ingresada";
 // alert(salida);
 
-let entrada = prompt("Ingresa tu edad aqui");
-let salida = "Tu edad es" + " " + entrada + " años.";
+// let entrada = prompt("Ingresa tu edad aqui");
+// let salida = "Tu edad es" + " " + entrada + " años.";
+// alert(salida);
+
+// let entrada = prompt("Ingresa tu nombre aqui");
+// let salida = "Hola" + " " + entrada + " " + "bienvenido al mundo Javascript!"
+// alert(salida);
+
+// let entrada = prompt("Ingrese su edad aqui");
+// let resultado = parseInt (entrada);
+// console.log(resultado);
+
+let entradaNombre = prompt("Ingrese su nombre");
+let entradaApellido = prompt("Ingrese su pellido");
+let salida = "Hola " + entradaNombre + " " + entradaApellido + " " + "bienvenido!"
 alert(salida);
+
