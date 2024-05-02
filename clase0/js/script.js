539084d39c3e042be6ca1b5fb276f666dfa3949e
julian martinez <julian1987m@gmail.com>
2024-05-02 10:22:50 -0300

    practica 0

---
+++ clase0/js/script.js
@@ -0,0 +1,7 @@
+// let entrada = prompt("Ingresa una letra");
+// let salida = entrada + " " + "ingresada";
+// alert(salida);
+
+let entrada = prompt("Ingresa tu edad aqui");
+let salida = "Tu edad es" + " " + entrada + " años.";
+alert(salida);
