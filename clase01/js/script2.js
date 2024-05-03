69f46564f1040e5b8c1f0775395ea647dd1a8603
julian martinez <julian1987m@gmail.com>
2024-05-03 10:44:52 -0300

    clase 1

---
+++ clase01/js/script2.js
@@ -0,0 +1,9 @@
+// PROMPT es un metodo que se usa para mostrar un cuadro de diálogo 
+// con un mensaje que solicita al usuario que ingrese algún texto o información.
+
+let primerNombre = prompt("Ingrese su Primer Nombre");
+// console.log(primerNombre);
+
+const NUMERO = prompt("Ingrese su Edad");
+alert("Su edad es " + NUMERO + " años");
+
